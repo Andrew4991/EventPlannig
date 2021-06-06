560b64953f1b41db2ab045ee4d1f27fc4254daf9
Andrew <uglik94@gmail.com>
2021-06-06 22:43:42 +0300

    added some tests

--- EventPlannig.Tests/BLL/EventRequiryServiceTests.cs
+++ EventPlannig.Tests/BLL/EventRequiryServiceTests.cs
@@ -4,6 +4,7 @@ using EventPlannig.DAL.Interfaces;
 using FluentAssertions;
 using Moq;
 using Xunit;
+
 namespace EventPlannig.Tests.BLL
 {
     public class EventRequiryServiceTests
@@ -24,6 +25,56 @@ namespace EventPlannig.Tests.BLL
             eventRequiry.Id.Should().Be(1);
         }
 
-      
+        [Fact]
+        public void Add_Always_Successfully()
+        {
+            // arrange
+            var eventRequiry = new EventRequiry { Id = 1 };
+            var mock = new Mock<IRepository<EventRequiry>>();
+
+            var eventRequiryService = new EventRequiryService(mock.Object);
+
+            // act
+            eventRequiryService.Add(eventRequiry);
+
+            // assert
+            mock.Verify(x => x.Add(eventRequiry));
+        }
+
+        [Fact]
+        public void Remove_Always_Successfully()
+        {
+            // arrange
+            var eventRequiry = new EventRequiry { Id = 1 };
+            var mock = new Mock<IRepository<EventRequiry>>();
+
+            var eventRequiryService = new EventRequiryService(mock.Object);
+
+            // act
+            eventRequiryService.Remove(eventRequiry);
+
+            // assert
+            mock.Verify(x => x.Remove(eventRequiry));
+        }
+
+        [Fact]
+        public void ChangeConfirmation_Always_Successfully()
+        {
+            // arrange
+            var eventRequiry = new EventRequiry { Id = 1 };
+            var mock = new Mock<IRepository<EventRequiry>>();
+
+            var eventRequiryService = new EventRequiryService(mock.Object);
+
+            // act
+            eventRequiryService.ChangeConfirmation(eventRequiry);
+
+            // assert
+            mock.Verify(x => x.Remove(eventRequiry));
+            eventRequiry.IsApproved = true;
+            eventRequiry.Id = 0;
+            mock.Verify(x => x.Add(eventRequiry));
+        }
+
     }
 }
---
+++ EventPlannig.Tests/BLL/EventServiceTests.cs
@@ -0,0 +1,64 @@
+﻿using EventPlannig.BLL.Services;
+using EventPlannig.DAL.Entities;
+using EventPlannig.DAL.Interfaces;
+using FluentAssertions;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace EventPlannig.Tests.BLL
+{
+    public class EventServiceTests
+    {
+        
+        [Fact]
+        public void Add_Always_Successfully()
+        {
+            // arrange
+            var _event = new Event { Id = 1 };
+            var mock = new Mock<IRepository<Event>>();
+
+            var eventService = new EventService(mock.Object);
+
+            // act
+            eventService.Add(_event);
+
+            // assert
+            mock.Verify(x => x.Add(_event));
+        }
+
+        [Fact]
+        public void Remove_Always_Successfully()
+        {
+            // arrange
+            var _event = new Event { Id = 1 };
+            var mock = new Mock<IRepository<Event>>();
+
+            var eventService = new EventService(mock.Object);
+
+            // act
+            eventService.Remove(_event);
+
+            // assert
+            mock.Verify(x => x.Remove(_event));
+        }
+
+        [Fact]
+        public void GetAll_Always_Successfully()
+        {
+            // arrange
+            var _event1 = new Event { Id = 1 };
+            var _event2 = new Event { Id = 1 };
+            var mock = new Mock<IRepository<Event>>();
+            mock.Setup(x => x.GetAll()).Returns(new List<Event> { _event1, _event2 });
+            var eventService = new EventService(mock.Object);
+
+            // act
+            var events = eventService.GetAll();
+
+            // assert
+            events.Should().BeEquivalentTo(new List<Event> { _event1, _event2 });
+        }
+
+    }
+}
